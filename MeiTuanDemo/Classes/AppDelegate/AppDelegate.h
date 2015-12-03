aeefc0e06013292457a1abda0a773c232bad53a8
lingtingSir <363905547@qq.com>
2015-12-03 23:27:05 +0800

    first
    12.3

---
+++ MeiTuanDemo/Classes/AppDelegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MeiTuanDemo
+//
+//  Created by lmj  on 15/12/3.
+//  Copyright (c) 2015年 lmj . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MeiTuanDemo/Classes/AppDelegate/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MeiTuanDemo
+//
+//  Created by lmj  on 15/12/3.
+//  Copyright (c) 2015年 lmj . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
