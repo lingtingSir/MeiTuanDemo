c8ceef8c8f9b1f2b35df3f16acad050a4136f6f6
lingtingSir <363905547@qq.com>
2015-12-04 17:44:08 +0800

    Update
    storyBoard

---
+++ MeiTuanDemo/BaseCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseCollectionViewController.h
+//  MeiTuanDemo
+//
+//  Created by lmj  on 15/12/4.
+//  Copyright (c) 2015年 lmj . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BaseCollectionViewController : UICollectionViewController
+
+@end
