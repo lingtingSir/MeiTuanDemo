26b81c4100e1944db3ec3645896b5194865c2a8d
lingtingSir <363905547@qq.com>
2015-12-04 23:45:40 +0800

    pod afn mjrefresh ...
    添加pch文件、pod 库

---
+++ MeiTuanDemo/Classes/Main/Controller/BaseController/BaseCollectionViewController.h
@@ -0,0 +1,15 @@
+//
+//  BaseCollectionViewController.h
+//  MeiTuanDemo
+//
+//  Created by lmj  on 15/12/4.
+//  Copyright (c) 2015年 lmj . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+@interface BaseCollectionViewController : UICollectionViewController
+
+@property (nonatomic,assign) BOOL shouldInitPullToRefresh;
+
+@end
---
+++ Podfile
@@ -0,0 +1,6 @@
+platform :ios, ‘8.0’
+
+pod “SDWebImage”, “~>3.7.3”
+pod “AFNetworking”, “~>2.5.3”
+pod “MJExtension”, “~>3.0.0”
+pod “MJRefresh”, “~>3.0.0” 
